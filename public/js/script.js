01f5a530eb05d9fedce415ab8a233f64e77f838b
Queen TiTi <trish@queen-titi.com>
2019-04-10 16:24:40 +0100

    all accepted

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,16 @@ let humidity = document.getElementById("humidity-div");
 
 searchButton.addEventListener("click", findWeatherDetails);
 searchInput.addEventListener("keyup", enterPressed);
+var spinner = new Spinner({
+	lines: 12, // The number of lines to draw
+	length: 7, // The length of each line
+	width: 5, // The line thickness
+	radius: 10, // The radius of the inner circle
+	color: '#000', // #rbg or #rrggbb
+	speed: 1, // Rounds per second
+	trail: 100, // Afterglow percentage
+	shadow: true // Whether to render a shadow
+}).spin(document.getElementById("ajaxContentHolder")); // Place in DOM node called "ajaxContentHolder"
 
 function enterPressed(event) {
   if (event.key === "Enter") {
